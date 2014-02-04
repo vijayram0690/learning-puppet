f7e88465953f4e74006afba1ac109dfcc1d9caed
xcargar <carlosags@fitec.org.br>
2014-02-04 20:14:31 +0000

    Adding facts and using classes

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,17 +1,36 @@
-exec { "apt-get update":
-  path => "/usr/bin",
+class hello_puppet {
+
+  exec { "apt-get update":
+    path => "/usr/bin",
+  }
+
+  package { "apache2":
+    ensure  => present,
+    require => Exec["apt-get update"],
+  }
+
+  service { "apache2":
+    ensure  => "running",
+    require => Package["apache2"],
+  }
+
+  file { "/var/www/hello-puppet":
+    ensure  => "link",
+    target  => "/vagrant/",
+    require => Package["apache2"],
+    notify  => Service["apache2"],
+  }
+
+  file {'facts.txt':
+      ensure  => file,
+      path    => '/vagrant/facts.txt',
+      mode    => 0644,
+      content => "This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
+hostname is ${fqdn}, but you might not be able to reach it there
+from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
+Puppet ${puppetversion}.",
+    }
 }
-package { "apache2":
-  ensure  => present,
-  require => Exec["apt-get update"],
-}
-service { "apache2":
-  ensure  => "running",
-  require => Package["apache2"],
-}
-file { "/var/www/hello-puppet":
-  ensure  => "link",
-  target  => "/vagrant/",
-  require => Package["apache2"],
-  notify  => Service["apache2"],
-}
+
+
+include 'hello_puppet'
