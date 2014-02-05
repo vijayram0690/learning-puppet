f27a8becb1f130af0e5d45c52ced9b055c2c12f9
xcargar <carlosags@fitec.org.br>
2014-02-05 18:53:28 +0000

    Adding hello puppet to CentOS/Amazon Linux

---
+++ 2_hello_puppet_centos/modules/hello_puppet/manifests/init.pp
@@ -0,0 +1,29 @@
+class hello_puppet {
+
+	$www_dir = "/var/www/html"
+
+	package { "httpd":
+		ensure	=> present,
+	}
+
+	service { "httpd":
+		ensure	=> "running",
+		require => Package["httpd"],
+	}
+
+	file { "${www_dir}/hello-puppet":
+		ensure	=> directory,
+		mode	=> 0644,
+		require => Package["httpd"],
+		notify	=> Service["httpd"],
+	}
+
+	file {"facts.txt":
+			ensure	=> file,
+			path		=> "${www_dir}/hello-puppet/facts.txt",
+			content => "This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
+hostname is ${fqdn}, but you might not be able to reach it there
+from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
+Puppet ${puppetversion}.",
+		}
+}
