03a12a42c4e2fda75a47f0724db9b582d0beeaca
root <root@ip-10-122-253-131.ec2.internal>
2014-02-06 16:24:19 +0000

    Configuring tomcat6 service

--- 3_cms2/modules/tomcat/manifests/init.pp
+++ 3_cms2/modules/tomcat/manifests/init.pp
@@ -2,14 +2,23 @@ class tomcat {
 
 	package { "tomcat6":
 		ensure	=> present,
+                notify  => Service["tomcat6"],
 	}
 
+	# TODO: remove these packages
 	package { "tomcat6-webapps":
 		ensure	=> present,
 		require	=> Package["tomcat6"],
+                notify  => Service["tomcat6"],
 	}
 
-	service { "tomcat":
+	package { "tomcat6-admin-webapps":
+		ensure	=> present,
+		require	=> Package["tomcat6"],
+		notify	=> Service["tomcat6"],
+	}
+
+	service { "tomcat6":
 		ensure	=> "running",
 		require => Package["tomcat6-webapps"],
 	}
