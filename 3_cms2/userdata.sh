d1eaa08fb8de5dcc2855ac3b6d409042da983fff
carlosgsouza <carlosgsouza@gmail.com>
2014-02-06 17:02:33 -0200

    configuring userdata for cloudformation tempalte

--- 3_cms2/userdata.sh
+++ 3_cms2/userdata.sh
@@ -1,1 +1,6 @@
 yum install -y puppet
+
+mkdir /puppet
+aws s2 cp s3://cmaas.dev.resources/infrastructure/puppet/ /puppet --region us-east-1
+
+puppet apply --modulepath=/puppet/modules  /puppet/manifests/default.pp
