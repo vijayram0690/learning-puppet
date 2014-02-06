792f67c15d30ec0b11373dd5d68ccf905f7c0fc0
carlosgsouza <carlosgsouza@gmail.com>
2014-02-06 19:37:28 -0200

    cms2 module now is media_check and introduced actual cms2 module

--- 3_cms2/modules/tomcat/manifests/init.pp
+++ 3_cms2/modules/tomcat/manifests/init.pp
@@ -1,5 +1,13 @@
 class tomcat {
 
+	package { "java-1.6.0-openjdk.x86_64":
+		ensure	=> absent,
+	}
+
+	package { "java-1.7.0-openjdk.x86_64":
+		ensure	=> present,
+	}
+
 	package { "tomcat7":
 		ensure	=> present,
 	}
