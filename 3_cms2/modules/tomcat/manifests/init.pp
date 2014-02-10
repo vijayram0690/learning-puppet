cc77792a637fc3673de58dfe7183baa1fd59ec5e
carlosgsouza <carlosgsouza@gmail.com>
2014-02-10 13:30:10 +0000

    removing java6

--- 3_cms2/modules/tomcat/manifests/init.pp
+++ 3_cms2/modules/tomcat/manifests/init.pp
@@ -1,15 +1,23 @@
 class tomcat {
 
-	package { "java-1.7.0-openjdk.x86_64":
-		ensure	=> present,
-	}
+        package { "java-1.7.0-openjdk.x86_64":
+                ensure  => present,
+                notify  => Service["tomcat7"],
+        }
 
-	package { "tomcat7":
-		ensure	=> present,
-	}
+        package { "java-1.6.0-openjdk.x86_64":
+                require => Package["tomcat7"],
+                ensure  => absent,
+                notify  => Service["tomcat7"],
+        }
 
-	service { "tomcat7":
-		ensure	=> "running",
-		require => Package["tomcat7"],
-	}
+        package { "tomcat7":
+                ensure  => present,
+        }
+
+        service { "tomcat7":
+                ensure  => "running",
+                require => Package["tomcat7"],
+        }
 }
+
