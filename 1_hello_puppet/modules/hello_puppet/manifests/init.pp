406fc25a0f94d78de731264f97b2b18d412064e5
vijayram0690 <vijay.ram0690@gmail.com>
2017-04-19 22:54:45 +0530

    Update init.pp

--- 1_hello_puppet/modules/hello_puppet/manifests/init.pp
+++ 1_hello_puppet/modules/hello_puppet/manifests/init.pp
@@ -26,7 +26,7 @@ class hello_puppet {
 	file {"facts.txt":
 			ensure	=> file,
 			path		=> "${www_dir}/hello-puppet/facts.txt",
-			content => "This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
+			content => "Hi Test This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
 hostname is ${fqdn}, but you might not be able to reach it there
 from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
 Puppet ${puppetversion}.",
